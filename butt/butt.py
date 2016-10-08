6ceae2d7d740cbc59f1b986515e15ea3ba5ca86e
sf17k <sf171k@gmail.com>
2016-10-07 23:35:21 -0400

    Initial commit

---
+++ butt/butt.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import random
+
+
+class Butt:
+    """Butt"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.lastMsgInChannel = dict()
+
+    @bot.event
+    async def on_message(self, message):
+        self.lastMsgInChannel[channel] = message
+        await bot.process_commands(message)
+
+    @bot.command(pass_context=True)
+    async def butt(self, ctx):
+        """butt"""
+
+        channel = ctx.message.channel
+        if channel not in lastMsgInChannel:
+            return
+        msg = self.lastMsgInChannel[channel].clean_content
+        words = msg.split()
+        index = random.randint(0, len(words) - 1)
+        words[index] = "butt"
+        await self.bot.say(str.join(words))
+
+
+def setup(bot):
+    bot.add_cog(Butt(bot))
