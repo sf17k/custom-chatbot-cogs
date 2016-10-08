cc62f3d7779628aa9232683a4ad91d7a809a2b80
sf17k <sf171k@gmail.com>
2016-10-08 00:14:59 -0400

    Fix 5

--- butt/butt.py
+++ butt/butt.py
@@ -12,7 +12,6 @@ class Butt:
 
     def on_message(self, message):
         self.lastMsgInChannel[channel] = message
-        await self.bot.process_commands(message)
 
     @commands.command(pass_context=True)
     async def butt(self, ctx):
